aa7c6429280767af3de291fe58eb4be464cfc60f
sevansoft <github@sevansoft.co.uk>
2022-03-02 00:49:21 +0000

    fix batch file

--- run-tests.bat
+++ run-tests.bat
@@ -6,8 +6,8 @@ msbuild.exe "FluentValidationTest.sln" /nologo /nr:false /p:platform="any cpu" /
 
 dotnet tool restore -v d
 
-dotnet test ".\tests\FluentValidationTest.Tests\FluentValidationTest.Tests.csproj" --no-build --configuration Release --collect:"xplat code coverage" --results-directory ./BuildReports/UnitTests/FluentValidationTest/net5.0 /p:CollectCoverage=true /p:CoverletOutput=..\..\BuildReports\Coverage\FluentValidationTest\net5.0\ /p:CoverletOutputFormat=cobertura /p:Exclude=\"[xunit.*]*"
+dotnet test ".\tests\FluentValidationTest.Tests\FluentValidationTest.Tests.csproj" --no-build --configuration Release --collect:"xplat code coverage" --results-directory ./BuildReports/UnitTests/FluentValidationTest/net6.0 /p:CollectCoverage=true /p:CoverletOutput=..\..\BuildReports\Coverage\FluentValidationTest\net6.0\ /p:CoverletOutputFormat=cobertura /p:Exclude=\"[xunit.*]*"
 
-reportgenerator "-reports:BuildReports\Coverage\FluentValidationTest\net5.0\coverage.cobertura.xml" "-targetdir:BuildReports\Coverage\FluentValidationTest\net5.0" -reporttypes:HtmlInline_AzurePipelines;Cobertura; -assemblyfilters:-xunit*.* -verbosity:Verbose
+reportgenerator "-reports:BuildReports\Coverage\FluentValidationTest\net6.0\coverage.cobertura.xml" "-targetdir:BuildReports\Coverage\FluentValidationTest\net6.0" -reporttypes:HtmlInline_AzurePipelines;Cobertura; -assemblyfilters:-xunit*.* -verbosity:Verbose
 
-start BuildReports\Coverage\FluentValidationTest\net5.0\index.htm
+start BuildReports\Coverage\FluentValidationTest\net6.0\index.htm
